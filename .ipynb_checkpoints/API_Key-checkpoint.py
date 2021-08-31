f1a0c0401bfbdeb4d9662c40a1135d834d240efe
taylorlast <taylorlast@gmail.com>
2021-08-31 09:49:52 -0400

    CFB update

---
+++ .ipynb_checkpoints/API_Key-checkpoint.py
@@ -0,0 +1,34 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "key =  \"IISPuGIU7/Noc2WD2sULhUoX4SzWSe1kT4o3a6+ZZN97CfHEhXWW4xoUkcNc7few\""
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
